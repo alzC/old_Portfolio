8bad1d645470690aca6fa2013ff4f742d4533438
angelo bouh <chimenbou.angelou@gmail.com>
2022-05-14 20:01:30 +0100

    deploy_test

---
+++ build/precache-manifest.3621ea2a18ac783b07c873e0af585806.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca278d2192de95c8c9a065f26be7d08a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ebc07aca52fe0bb2c58",
+    "url": "/static/css/main.30475116.chunk.css"
+  },
+  {
+    "revision": "e48ff8b65c124fcf5be9",
+    "url": "/static/js/2.287b021f.chunk.js"
+  },
+  {
+    "revision": "baebdd1671010f55d6a3b55cb359d81e",
+    "url": "/static/js/2.287b021f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ebc07aca52fe0bb2c58",
+    "url": "/static/js/main.73c42648.chunk.js"
+  },
+  {
+    "revision": "b97e93e0d520ef838460",
+    "url": "/static/js/runtime-main.4c4c3a36.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "e84c205576ce51cc6a90653c49df11f0",
+    "url": "/static/media/curology-min.e84c2055.png"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "ea0dc5634b4fc384f2cc571b1062285e",
+    "url": "/static/media/up-arrow-circle.ea0dc563.svg"
+  },
+  {
+    "revision": "5f9e30771ee4dbbad1689af16cb49028",
+    "url": "/static/media/yourspace-min.5f9e3077.png"
+  }
+]);
